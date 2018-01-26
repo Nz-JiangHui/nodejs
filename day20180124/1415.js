1c5ad2af54f06ed2f7a684d82d49631bebe2a345
jianghui <nz.jianghui@gmail.com>
2018-01-26 14:57:51 +0800

    add

---
+++ day20180124/1415.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Nz on 2018/1/24.
+ */
+var Client = require('ssh2').Client;
+
+var conn = new Client();
+conn.on('ready', function() {
+    console.log('Client :: ready');
+    conn.exec('./release.sh', function(err, stream) {
+        if (err) throw err;
+        stream.on('close', function(code, signal) {
+            console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+            conn.end();
+        }).on('data', function(data) {
+            console.log('STDOUT: ' + data);
+        }).stderr.on('data', function(data) {
+            console.log('STDERR: ' + data);
+        });
+    });
+}).connect({
+    host: '',
+    port: 22,
+    username: 'root',
+    password: ''
+});
